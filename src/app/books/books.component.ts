0fab81af0c5af7382edfe5c2d7772f522cdd0e16
MSIsu-MEAN <iswaryams97@gmail.com>
2024-10-07 14:27:43 +0530

    Update books.component.ts

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -19,22 +19,27 @@ export class BooksComponent implements OnInit {
   itemsPerPage: number = 5;
 
   //Book List
-  obj: any = {};
   booksList: any;
   editData: any = {};
 
   //search
   query: string = '';
 
+  //signIn
+  obj: any = {};
+  tocheck: boolean = false;
+  status: any;
+
 
   constructor(private apiService: ServiceService, public bsModalRef: BsModalRef, public toastrService: ToastrService, private changeDetectorRef: ChangeDetectorRef, private formBuilder: FormBuilder) {
     this.myForm = this.formBuilder.group({
-      title: ['',[ Validators.required]],
-      author: ['',[ Validators.required]],
-      description: ['',[ Validators.required]],
+      title: ['', [Validators.required]],
+      author: ['', [Validators.required]],
+      description: ['', [Validators.required]],
       publicationYear: ['', [Validators.required, Validators.min(1000), Validators.max(2024)]],
-      ISBN: ['',[ Validators.required]],
+      ISBN: ['', [Validators.required]],
     });
+
     this.editForm = this.formBuilder.group({
       title: [''],
       author: [''],
@@ -46,8 +51,34 @@ export class BooksComponent implements OnInit {
 
   ngOnInit(): void {
     this.getList()
+    this.status = localStorage.getItem('login')
   }
 
+  submit(form: any) {
+    this.tocheck = true
+    if (form.valid) {
+      console.log("obj", this.obj);
+      this.apiService.postData('signIn', this.obj).subscribe((res) => {
+        if (res.status) {
+          localStorage.setItem('login', 'true')
+          this.toastrService.success(res.message, 'Success')
+          form.reset();
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+          this.tocheck = false
+        }
+      })
+    }
+    else {
+      form.reset()
+      this.toastrService.error("Please enter the required fields", 'Error')
+    }
+  }
+
+
+
+  //adding book function
   onSubmit() {
     if (this.myForm.valid) {
       this.apiService.postData('addBooks', this.myForm.value).subscribe(async (response: any) => {
@@ -61,7 +92,6 @@ export class BooksComponent implements OnInit {
         }
         else {
           this.toastrService.error(response.message, 'Error');
-          this.myForm.reset();
         }
       });
     }
@@ -70,6 +100,7 @@ export class BooksComponent implements OnInit {
     }
   }
 
+  //get all list
   getList() {
     this.apiService.postData('booksList', {}).subscribe((list: any) => {
       this.booksList = list.data
@@ -77,6 +108,7 @@ export class BooksComponent implements OnInit {
     })
   }
 
+  //getting particular datas
   getParticulars(id: any) {
     this.apiService.postData('particularList', { ISBN: id }).subscribe((getList: any) => {
       this.editData = getList?.data[0]
@@ -90,6 +122,7 @@ export class BooksComponent implements OnInit {
     })
   }
 
+  //update function
   updates() {
     this.editData = { ...this.editData, ...this.editForm.value };
     this.editData.id = this.editData._id
@@ -118,7 +151,7 @@ export class BooksComponent implements OnInit {
     }
   }
 
-
+  //delete function
   delete(id: any) {
     this.apiService.postData('deleteBooks', { ISBN: id }).subscribe((deleted: any) => {
       if (deleted.status) {
